a5547a87c9c56f606b0e8cd92cfb284d4324aa03
sgillet1007 <samuelgillet@hotmail.com>
2015-07-13 16:10:36 -0600

    html input elements added

--- lf1.js
+++ lf1.js
@@ -3,12 +3,16 @@ $(document).on('ready', function(){
 var map = L.map('leaflet-map').setView([15,-68],4);
 // vv*****  GLOBAL VARIABLES *********************
 var diveTally = 1;
+var mapEdit = false;
+
 var myDivePts = L.geoJson(); //myDivePts is a geoJson layer
 
 myDivePts.addTo(map);
 
 // ^^*********************************************
+
 map.on('click', onMapClick);
+
 // vv***** load tile layer *************
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -18,38 +22,43 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
 // ^^*********************************************
 
-//vv***************** start onMapClick() function ***********************
+//vv*** start onMapClick() function **************
 function onMapClick(e) {
-    // if (editMap){
-    console.log(e.latlng);
+        // show input form using JQ
+    
+    $('.form-wrapper').css('display','inline-block');
+
     var diveNo = "Dive #" + diveTally;
     var date = $('.input-date').val();
-    console.log(date);
-    // create a geoJson object called currentDive
+        // TO DO add remaining variables to pass to object properties below...
+        
+        // create a geoJson object called currentDive
     var currentDive = {
         "type":"Feature",
         "properties": {
             "id": diveNo,
             "popupContent": diveNo +'!',
             "date": date,
+                // TO DO add remaining properties
         },
         "geometry": {
             "type":"Point",
-            "coordinates": [e.latlng.lng,e.latlng.lat]
+            "coordinates": [e.latlng.lng,e.latlng.lat],
         }
     }
-    // add currentDive to myLayer
+        // add currentDive to myLayer
     myDivePts.addData(currentDive);
 
     // Adds .log-entry div to #logbook with content = diveTally
+    // TO DO: only do JQ below on submit button on.click...
     $('#log-book').append('<div class="log-entry">'+ currentDive.properties.id 
-                            +'<br>Date: '+currentDive.properties.date
-                            +'<br>Lat: '+ currentDive.geometry.coordinates[1]
-                            + '<br>Long: '+currentDive.geometry.coordinates[0]
-                            +'</div>');
+                                +'<br>Date: '+currentDive.properties.date
+                                +'<br>Lat: '+ currentDive.geometry.coordinates[1]
+                                + '<br>Long: '+currentDive.geometry.coordinates[0]
+                                +'</div>');
     ++diveTally;
-// }
 }; //******************* ^^^ end onMapClick() function ^^^ ***********************
+
 // Zoom buttone JQ click handlers ******************vv
 $('.default-zoom').on('click',function(){
     map.setView([15,-68],4);
