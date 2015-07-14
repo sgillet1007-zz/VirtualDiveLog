038bfd77a49ce244474d98f660f405b97402c4c7
sgillet1007 <samuelgillet@hotmail.com>
2015-07-13 18:39:18 -0600

    geoSon properties assigned to input vals

--- lf1.js
+++ lf1.js
@@ -1,45 +1,62 @@
 $(document).on('ready', function(){
 // initialize the map
 var map = L.map('leaflet-map').setView([15,-68],4);
-// vv*****  GLOBAL VARIABLES *********************
-var diveTally = 1;
-var mapEdit = false;
 
+// initialize global diveTally
+var diveTally = 1;
+
+// initialize empty geoJson
 var myDivePts = L.geoJson(); //myDivePts is a geoJson layer
 
+// add geoJson layer to map
 myDivePts.addTo(map);
 
-// ^^*********************************************
-
-map.on('click', onMapClick);
-
-// vv***** load tile layer *************
+// vv***** load mapbox tile layer *************
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'sgillet1007.1e5d6005',
     accessToken: 'pk.eyJ1Ijoic2dpbGxldDEwMDciLCJhIjoiMzQ4ZWE5YWNlZDQ3NmEzMzY0ZTk4ZDc2MWJjMWFjMDkifQ.M3cCfmaaAVzdYQfQ3fIDoQ'
 }).addTo(map);
-// ^^*********************************************
+
+// runs onMapClick when map is clicked
+map.on('click', onMapClick);
 
 //vv*** start onMapClick() function **************
 function onMapClick(e) {
-        // show input form using JQ
-    
+    // display input form
     $('.form-wrapper').css('display','inline-block');
 
-    var diveNo = "Dive #" + diveTally;
+    var id = diveTally;
+    var diveString = "Dive #" + diveTally;
     var date = $('.input-date').val();
-        // TO DO add remaining variables to pass to object properties below...
+    var maxDepth =$('.input-maxDepth').val();
+    var viz = $('.input-viz').val();
+    var wTemp = $('.input-wTemp').val();
+    var timeIn = $('.input-timeIn').val();
+    var timeOut = $('.input-timeOut').val();
+    var psiStart = $('.input-psiStart').val();
+    var psiEnd = $('.input-psiEnd').val();
+    var verifNo = $('.input-verifNo').val();
+    var saltW = $('.input-salt').val();
+    var freshW = $('.input-fresh').val();
         
         // create a geoJson object called currentDive
     var currentDive = {
         "type":"Feature",
         "properties": {
-            "id": diveNo,
-            "popupContent": diveNo +'!',
+            "id": diveString,
+            "popupContent": diveString,
             "date": date,
-                // TO DO add remaining properties
+            "maxDepth": maxDepth,
+            "viz": viz,
+            "wTemp": wTemp,
+            "timeIn":timeIn,
+            "timeOut":timeOut,
+            "psiStart":psiStart,
+            "verifNo":verifNo,
+            "saltW":saltW,
+            "freshW":freshW,
         },
         "geometry": {
             "type":"Point",
@@ -51,12 +68,13 @@ function onMapClick(e) {
 
     // Adds .log-entry div to #logbook with content = diveTally
     // TO DO: only do JQ below on submit button on.click...
-    $('#log-book').append('<div class="log-entry">'+ currentDive.properties.id 
-                                +'<br>Date: '+currentDive.properties.date
-                                +'<br>Lat: '+ currentDive.geometry.coordinates[1]
-                                + '<br>Long: '+currentDive.geometry.coordinates[0]
-                                +'</div>');
+    // $('#log-book').append('<div class="log-entry">'+ currentDive.properties.popupContent 
+    //                             +'<br>Date: '+currentDive.properties.date
+    //                             +'<br>Lat: '+ currentDive.geometry.coordinates[1]
+    //                             + '<br>Long: '+currentDive.geometry.coordinates[0]
+    //                             +'</div>');
     ++diveTally;
+    console.log(myDivePts)
 }; //******************* ^^^ end onMapClick() function ^^^ ***********************
 
 // Zoom buttone JQ click handlers ******************vv
