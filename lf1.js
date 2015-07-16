ac93e3af14cc8c952138db21e0ecf347f8bccd09
sgillet1007 <samuelgillet@hotmail.com>
2015-07-15 19:48:20 -0600

    all but logbook entries works

--- lf1.js
+++ lf1.js
@@ -5,6 +5,16 @@ var map = L.map('leaflet-map').setView([15,-68],4);
 // initialize global diveTally
 var diveTally = 1;
 
+//initialize global diveTimeTotal for hours dived
+var diveTimeTot = 0;
+//version of diveTime global rounded to tenths
+var diveTimeTotR = 0;
+
+//initialize array for air consumption values
+var airConsArray = [];
+//initialized global airConsumpAvg for avg 
+var airConsAvg = 0;
+
 map.on('click', onMapClick);
 
 // create empty array to place currentDive objects into
@@ -29,6 +39,7 @@ var diveTime = function(timeIn,timeOut){
     var diveMins = endMins - startMins;
     return diveMins;
 }
+
 // psi consumed helper function. inputs start and end psi strings. returns a number (psi used).
 var psiUsed = function(psiIn, psiOut){
     var psiIn = Number(psiIn);
@@ -37,10 +48,26 @@ var psiUsed = function(psiIn, psiOut){
     return psiUsed;
 }
 
+// avg air consumption helper consumption
+var arrayAvg = function(array){
+    var airSum = 0;
+    for (var i=0;i<array.length;i++){
+        airSum += array[i];
+    }
+    var output = Math.round(airSum/diveTally);
+    return output
+}
+
+//helper function to round to one decimal place
+var roundTenths = function(num){
+    var output = Math.round(num*10)/10;
+    return output
+}
+
 // listner for New Dive button click
 $('.new-form-button').on('click',function(){
         // shows form when New Dive button is clicked
-        $('.form-wrapper').css('display','block');
+        $('.form-wrapper').show(1000);
         // reset form input values
         $('.input-date').val(undefined);
         $('.input-location').val(undefined);
@@ -70,7 +97,9 @@ function onMapClick(e) {
         var saltW = $('.input-salt').val(); 
         var notes =$('.input-notes').val();
         var diveMins = diveTime(timeIn,timeOut);
-        var psiConsumed = psiUsed(psiStart,psiEnd);  
+        var psiConsumed = psiUsed(psiStart,psiEnd); 
+        var diveHours = (diveMins/60);
+        var diveAirCons = (psiConsumed/diveMins);
        
     // create a geoJson object called currentDive
     var currentDive = {
@@ -93,6 +122,8 @@ function onMapClick(e) {
             "notes":notes,
             "diveMins":diveMins,
             "psiUsed":psiConsumed,
+            "diveHours":diveHours,
+            "diveAirCons":diveAirCons,
         },
         "geometry": {
             "type":"Point",
@@ -103,35 +134,57 @@ function onMapClick(e) {
     myDiveArray.push(currentDive);
     var marker = L.marker([e.latlng.lat,e.latlng.lng]).addTo(map);
     marker.bindPopup(""+currentDive.properties.popupContent).openPopup();
-    console.log(myDiveArray);
-    
+
+
+    airConsArray.push(currentDive.properties.diveAirCons);
+    airConsAvg = arrayAvg(airConsArray);
+
+    diveTimeTot += currentDive.properties.diveHours;
+    diveTimeTotR = roundTenths(diveTimeTot);
+
+    if (diveTally === 1){
+        $('.diveTime-stat').after('<p class="stats-val dt">' + ' ' + diveTimeTotR + '</p>');
+        $('.airCons-stat').after('<p  class="stats-val ac">' + ' ' + airConsAvg + '</p>');
+    }
+    else {
+        $('.dt').replaceWith('<p class="stats-val dt">' + ' ' + diveTimeTotR + '</p>');
+        $('.ac').replaceWith('<p  class="stats-val ac">' + ' ' + airConsAvg + '</p>');
+    }
+
     ++diveTally;
 
+    $('#log-book').append('<div class="log-entry">'+ currentDive.properties.popupContent +'</div>');
+
     // hide form
-    $('.form-wrapper').css('display','none');
+    $('.form-wrapper').hide(1000);
+
+    console.log('lat: ' + e.latlng.lat + 'long: ' + e.latlng.lng)
 }; //******************* ^^^ end onMapClick() function ^^^ ***********************
 
-
-
 // Zoom button JQ click handlers ******************vv
-// $('.default-zoom').on('click',function(){
-//     map.setView([15,-68],4);
-// })
-// $('.coz-zoom').on('click',function(){
-//     map.setView([20.437307950568957,-86.91352844238281],10);
-// })
-// $('.global-zoom').on('click',function(){
-//     map.setView([34.30714385628804,-19.335937499999996],2);
-// })
+$('.default-zoom').on('click',function(){
+    map.setView([15,-68],4);
+})
+$('.coz-zoom').on('click',function(){
+    map.setView([20.437307950568957,-86.91352844238281],10);
+})
+$('.global-zoom').on('click',function(){
+    map.setView([5,-1],2);
+})
+$('.gbr-zoom').on('click',function(){
+    map.setView([-18.7295,150.8642],5);
+})
+$('.cuba-zoom').on('click',function(){
+    map.setView([20.437307950568957,-75],6);
+})
+$('.cura-zoom').on('click',function(){
+    map.setView([12.17,-68.7],8);
+})
 // *************************************************^^
 
  // console.log(myDivePts);
     // Adds .log-entry div to #logbook with content = diveTally
     // TO DO: only do JQ below on submit button on.click...
-    // $('#log-book').append('<div class="log-entry">'+ currentDive.properties.popupContent 
-    //                             +'<br>Date: '+currentDive.properties.date
-    //                             +'<br>Lat: '+ currentDive.geometry.coordinates[1]
-    //                             + '<br>Long: '+currentDive.geometry.coordinates[0]
-    //                             +'</div>');
+   
 
 })
